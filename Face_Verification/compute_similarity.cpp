41267168c991c4eb3f11314354497549085095c1
SeungKweon Yoo <yoosk526@naver.com>
2022-11-17 22:52:30 +0900

    Create compute_similarity.cpp

---
+++ Face_Verification/compute_similarity.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#define COSINE
+//#define EUCLIDEAN
+
+float compute_similarity(float* ref, float* tar) {
+	int dim = binNum * blockNum;
+	float score;
+
+#ifdef COSINE
+	int k;
+	float nomi = 0, denomi;
+	float refMag = 0, tarMag = 0;
+
+	for (k = 0; k < dim; k++) {
+		nomi += ref[k] * tar[k];
+		refMag += ref[k] * ref[k];
+		tarMag += tar[k] * tar[k];
+	}
+	denomi = sqrt(refMag) * sqrt(tarMag);
+	score = nomi / denomi;
+
+#endif
+
+	return exp(6 * score);
+}
