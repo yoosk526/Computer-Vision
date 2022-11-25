8d3cfc3d8eb8e70976b86d6aaca8b98f04c19e05
SeungKweon Yoo <yoosk526@naver.com>
2022-11-25 19:44:43 +0900

    SLIC superpixel

---
+++ SLIC_superpixel/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "main.h"
+
+void main() {
+	Mat image = imread("../../../media/edge_building_3.jpg", 1);
+	resize(image, image, Size(image.cols * 0.5, image.rows * 0.5));
+
+	SLICsegmentation(image);
+}
---
+++ SLIC_superpixel/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+void SLICsegmentation(Mat image);
